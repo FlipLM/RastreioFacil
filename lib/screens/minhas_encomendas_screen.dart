5ff9a62240bcb07c42925fa0ca8a903da32a0ec7
FilipeLM <36077261+FlipLM@users.noreply.github.com>
2023-11-02 10:33:54 -0300

    alterações

--- lib/screens/minhas_encomendas_screen.dart
+++ lib/screens/minhas_encomendas_screen.dart
@@ -15,7 +15,7 @@ class MinhasEncomendasScreen extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Image.asset('lib/assets/imagens/Mailbox-bro.png'), // Substitua 'URL_DA_IMAGEM' pela URL da imagem que você quer mostrar
+                Image.asset('lib/assets/imagens/Mailbox-bro.png'),
                 const Text(
                   'Você não tem pacotes adicionados.',
                   style: TextStyle(
--- lib/widgets/encomenda_widget.dart
+++ lib/widgets/encomenda_widget.dart
@@ -13,44 +13,49 @@ class EncomendaWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)), // Aumente o valor para arredondar mais as bordas
       elevation: 5.0,
-      child: ListTile(
-          contentPadding: const EdgeInsets.all(10.0),
-          tileColor: Colors.grey[200],
-          title:
-          Text(encomenda.nome, style:
-          const TextStyle(fontWeight:
-          FontWeight.bold, fontSize:
+      child:
+      ListTile(
+        contentPadding: const EdgeInsets.all(20.0),
+        tileColor: Colors.grey[200],
+        title:
+        Padding( // Adicione este widget
+          padding: const EdgeInsets.only(bottom: 15.0), // Ajuste o valor da margem conforme necessário
+          child: Text(encomenda.nome, style:
+          const TextStyle(
+              fontWeight:
+              FontWeight.bold, fontSize:
           18.0)),
-          subtitle:
-          Text('Transportadora: ${encomenda.transportadora}\nCódigo de Rastreio: ${encomenda.codigoRastreio}'),
-              trailing:
-              IconButton(icon:
-              const Icon(Icons.delete, color:
-              Colors.red), onPressed:
-          () {
-        Provider.of<EncomendaModel>(context, listen:
-        false).removerEncomenda(encomenda);
-      }),
-      onTap: () async {
-        Clipboard.setData(ClipboardData(text:
-        encomenda.codigoRastreio));
-        String? url =
-        transportadoras.firstWhere((t) =>
-        t['name'] ==
-            encomenda.transportadora)['transportadoraUrl'];
-        Navigator.push(context, MaterialPageRoute(builder:
-            (context) =>
-            WebViewScreen(url!)));
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Código de rastreio copiado!'),
-          ),
-        );
-      },
-    ),
+        ),
+        subtitle:
+        Text('Transportadora: ${encomenda.transportadora}\nCódigo de Rastreio: ${encomenda.codigoRastreio}'),
+        trailing:
+        IconButton(icon:
+        const Icon(Icons.delete, color:
+        Colors.grey), onPressed:
+            () {
+          Provider.of<EncomendaModel>(context, listen:
+          false).removerEncomenda(encomenda);
+        }),
+        onTap: () async {
+          Clipboard.setData(ClipboardData(text:
+          encomenda.codigoRastreio));
+          String? url =
+          transportadoras.firstWhere((t) =>
+          t['name'] ==
+              encomenda.transportadora)['transportadoraUrl'];
+          Navigator.push(context, MaterialPageRoute(builder:
+              (context) =>
+              WebViewScreen(url!)));
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Código de rastreio copiado!'),
+            ),
+          );
+        },
+      ),
     );
   }
 }
